30d1a88f7ab69384a92cb5c1b1d57bbccef30543
Prakash2698 <prakashv2698@gmail.com>
2023-11-06 11:01:37 +0530

    i

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,7 +9,6 @@ const authenticateToken = (req, res, next) => {
   if (!token) {
     return res.status(401).json({ message: 'No token provided' });
   }
-
   jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
     if (err) {
       return res.status(403).json({ message: 'Failed to authenticate token' });
